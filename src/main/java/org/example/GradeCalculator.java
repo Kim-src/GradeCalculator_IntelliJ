950ab5d7faf6d14338afe92d2c3059241e956bca
Chang-Seong Kim <ceobaek153@gmail.com>
2024-03-09 18:31:21 +0900

    Created KimGradeCalculator

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final Courses courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+    public GradeCalculator(Courses courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        // (학점수 * 교과목 평점)의 합계
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+        // 수강신청 총학점 수
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
+
+    }
+}
---
+++ src/test/java/org/example/CourseTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CourseTest {
+
+    /*
+    - 요구사항
+    - 평균학점 계산 방법 = (학점수 * 교과목 평점)의 합계 / 수강신청 총학점 수
+    - 일급 컬렙션 사용
+     */
+    
+    @DisplayName("평균 학점을 계산한다.")
+    @Test
+    void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"));
+                new Course("자료구조", 3, "A+");
+
+        GradeCalculator gradeCalculator = new GradeCalculator(new Courses(courses));
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
+    }
+
+}
